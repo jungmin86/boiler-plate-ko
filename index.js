4c62227ed6b6592c92053cdad0156a507bcc9f78
jungmin86 <jmkimm0p@gmail.com>
2022-08-27 23:20:04 +0900

    영창 박스 살려줘

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models.User")
+
+app.use(express.urlencoded({ extended: true }));
+
+//app.use(bodyParser.json());
+
+app.use(express.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jungmin:sire09@cluster98.lqvzyon.mongodb.net/?retryWrites=true&w=majority', {
@@ -9,7 +18,21 @@ mongoose.connect('mongodb+srv://jungmin:sire09@cluster98.lqvzyon.mongodb.net/?re
   .catch(err => console.log(err))
 
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+
+app.post('/register', (req,res) => {
+   // 회원가입할때 필요한 정보들을 client에서 가져오면
+   //그것들을 데이터베이스에 넣어준다.
+
+   const user = new User(req.body);
+
+   user.save((err, userInfo) => {
+     if (err) return res.json({ success: false, err })
+     return res.status(200).json({
+       success: true
+     })
+   })
+ })
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -33,4 +33,5 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = { User }
+//module.exports = { User }
+export default User;
